2297cc6af4d12e39dc5b7bb12190732e0a78c787
1owol <w3162844944@126.com>
2023-01-21 18:16:14 +0800

    1

--- 直接选择排序,冒泡排序，快速排序/直接选择排序,冒泡排序，快速排序/sort.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include<stdio.h>
-#include<stdlib.h>
-void StraightSelectSort(int* a, int n);
-void BubbleSort(int* a, int n);
-void QuickSort1(int* a, int left, int right);
-int MidNumber(int* a, int left, int right);
-void QuickSort2(int* a, int left, int right);
-void QuickSort3(int* a, int left, int right);
